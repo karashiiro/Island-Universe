251289fb87728cea947625e8e80eba1527afe387
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-22 09:22:20 -0700

    mostly finish styling

--- IslandUniverse/Services/Core/UiManagerService.cs
+++ IslandUniverse/Services/Core/UiManagerService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Drawing;
 using System.IO;
 using System.Numerics;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using static ImGuiScene.SimpleImGuiScene;
@@ -30,7 +31,7 @@ namespace IslandUniverse.Services.Core
             {
                 Title = "ImGui Overlay",
                 Fullscreen = true,
-                TransparentColor = new float[] { 0, 0, 0 },
+                TransparentColor = new float[] { 0, 0, 0, 0 },
             })
             {
                 ImGuiIniPath = iniPath,
@@ -46,17 +47,46 @@ namespace IslandUniverse.Services.Core
                 fontConfig.GlyphExtraSpacing.X = 0.5f;
                 
                 var io = ImGui.GetIO();
-                io.Fonts.AddFontFromFileTTF(@"Assets\Roboto-Medium.ttf", 14f, fontConfig);
+
+                var defaultFontStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("IslandUniverse.Assets.Ubuntu-Regular.ttf");
+                var dfIntPtr = Marshal.AllocHGlobal((int)defaultFontStream.Length);
+                var defaultFont = new UnmanagedMemoryStream((byte*)dfIntPtr.ToPointer(), defaultFontStream.Length, defaultFontStream.Length, FileAccess.Write);
+                defaultFontStream.CopyTo(defaultFont);
+
+                io.Fonts.AddFontFromMemoryTTF(dfIntPtr, 13, 13f, fontConfig);
                 io.Fonts.Build();
 
                 fontConfig.Destroy();
             }
 
+            // Copy Dalamud's style, it's good
             var style = ImGui.GetStyle();
             style.GrabRounding = 3f;
             style.FrameRounding = 4f;
             style.WindowRounding = 4f;
             style.WindowBorderSize = 0f;
+
+            style.Colors[(int)ImGuiCol.WindowBg] = new Vector4(0.06f, 0.06f, 0.06f, 0.87f);
+            style.Colors[(int)ImGuiCol.FrameBg] = new Vector4(0.29f, 0.29f, 0.29f, 0.54f);
+            style.Colors[(int)ImGuiCol.FrameBgHovered] = new Vector4(0.54f, 0.54f, 0.54f, 0.40f);
+            style.Colors[(int)ImGuiCol.FrameBgActive] = new Vector4(0.64f, 0.64f, 0.64f, 0.67f);
+            style.Colors[(int)ImGuiCol.TitleBgActive] = new Vector4(0.29f, 0.29f, 0.29f, 1.00f);
+            style.Colors[(int)ImGuiCol.TitleBgCollapsed] = new Vector4(0.19f, 0.19f, 0.19f, 1.00f); // Minor tweak so this doesn't go transparent when collapsed
+            style.Colors[(int)ImGuiCol.CheckMark] = new Vector4(0.86f, 0.86f, 0.86f, 1.00f);
+            style.Colors[(int)ImGuiCol.SliderGrab] = new Vector4(0.54f, 0.54f, 0.54f, 1.00f);
+            style.Colors[(int)ImGuiCol.SliderGrabActive] = new Vector4(0.67f, 0.67f, 0.67f, 1.00f);
+            style.Colors[(int)ImGuiCol.Button] = new Vector4(0.71f, 0.71f, 0.71f, 0.40f);
+            style.Colors[(int)ImGuiCol.ButtonHovered] = new Vector4(0.47f, 0.47f, 0.47f, 1.00f);
+            style.Colors[(int)ImGuiCol.ButtonActive] = new Vector4(0.74f, 0.74f, 0.74f, 1.00f);
+            style.Colors[(int)ImGuiCol.Header] = new Vector4(0.59f, 0.59f, 0.59f, 0.31f);
+            style.Colors[(int)ImGuiCol.HeaderHovered] = new Vector4(0.50f, 0.50f, 0.50f, 0.80f);
+            style.Colors[(int)ImGuiCol.HeaderActive] = new Vector4(0.60f, 0.60f, 0.60f, 1.00f);
+            style.Colors[(int)ImGuiCol.ResizeGrip] = new Vector4(0.79f, 0.79f, 0.79f, 0.25f);
+            style.Colors[(int)ImGuiCol.ResizeGripHovered] = new Vector4(0.78f, 0.78f, 0.78f, 0.67f);
+            style.Colors[(int)ImGuiCol.ResizeGripActive] = new Vector4(0.88f, 0.88f, 0.88f, 0.95f);
+            style.Colors[(int)ImGuiCol.Tab] = new Vector4(0.23f, 0.23f, 0.23f, 0.86f);
+            style.Colors[(int)ImGuiCol.TabHovered] = new Vector4(0.71f, 0.71f, 0.71f, 0.80f);
+            style.Colors[(int)ImGuiCol.TabActive] = new Vector4(0.36f, 0.36f, 0.36f, 1.00f);
 
             this.scene.Run();
         }
